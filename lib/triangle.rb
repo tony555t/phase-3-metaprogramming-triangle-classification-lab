350da4d72db28444f1fd14c146a4e04c541384cc
tony555t <tonytullah764@gmail.com>
2023-02-16 13:14:22 +0300

    metaprograming

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    valid_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |s|
+      valid_triangle << false if s <= 0
+    end
+    raise TriangleError if valid_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+    def message
+      "invalid triangle"
+    end
+  end
 end
+tri = Triangle.new(7,7,0)
+binding.pry
